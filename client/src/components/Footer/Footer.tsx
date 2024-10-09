e7ba117b6b35991510818f0437363c95217b8ae9
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-10-09 23:58:40 +0200

    Created footer

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  const fields = [
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+          width={24}
+          height={24}
+        >
+          <path
+            fill-rule="evenodd"
+            d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      ),
+      text: "Adresa: Jovana Jovanica Zmaja 20",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+          width={24}
+          height={24}
+        >
+          <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+          <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+        </svg>
+      ),
+      text: "E-mail: uros.n.nikolic123@gmail.com",
+    },
+    {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-6"
+          width={24}
+          height={24}
+        >
+          <path
+            fill-rule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      ),
+      text: "Radimo: ponedeljak-petak 00-24",
+    },
+  ];
+
+  return (
+    <footer className={styles.footer}>
+      <div className={`${styles.footerContentWrapper} container`}>
+        {fields.map((field, index) => (
+          <React.Fragment key={index}>
+            <div className={styles.footerFieldContent}>
+              {field.icon}
+              <p>{field.text}</p>
+            </div>
+            {index < fields.length - 1 && (
+              <div className={styles.separator}></div>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
+    </footer>
+  );
+}
