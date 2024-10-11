4f50a3a7b40a85ce346df0d1386d5834f8cc7675
urosnikolic123 <uros.n.nikolic123@gmail.com>
2024-10-11 15:44:04 +0200

    text changes, and hamburger menu animation

--- client/src/components/HamburgerMenu/HamburgerMenu.tsx
+++ client/src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -45,60 +45,58 @@ export default function HamburgerMenu({ navLinks }: HamburgerMenuProps) {
 
   return (
     <>
-      {isOpened ? (
-        <div className={styles.menuWrapper}>
-          <div className={styles.menuHeading}>
-            <a href={`tel:${PHONE_NUMBER}`}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="#fbd858"
-                className="size-6"
-                height={35}
-                width={35}
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              {PHONE_NUMBER}
-            </a>
-            {closeIcon}
-          </div>
-          <div className={styles.menuInBetween}>
-            <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
-          </div>
-          <ul className={styles.menuList}>
-            {navLinks.map((navLink, index) => (
-              <li className={styles.menuItem} key={index}>
-                <a href={`#${navLink.href}`} onClick={() => setIsOpened(false)}>
-                  <div className={styles.menuLabel}>
-                    {navLink.icon}
-                    <p>{navLink.label}</p>
-                  </div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#fbd858"
-                    className="size-6"
-                    height={35}
-                    width={35}
-                    aria-label="Navigate to section"
-                  >
-                    <path strokeLinecap="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                  </svg>
-                </a>
-              </li>
-            ))}
-          </ul>
+      {openIcon}
+      <div className={`${styles.menuWrapper} ${isOpened ? styles.open : ""}`}>
+        <div className={styles.menuHeading}>
+          <a href={`tel:${PHONE_NUMBER}`}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="#fbd858"
+              className="size-6"
+              height={35}
+              width={35}
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {PHONE_NUMBER}
+          </a>
+          {closeIcon}
         </div>
-      ) : (
-        openIcon
-      )}
+        <div className={styles.menuInBetween}>
+          <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+        </div>
+        <ul className={styles.menuList}>
+          {navLinks.map((navLink, index) => (
+            <li className={styles.menuItem} key={index}>
+              <a href={`#${navLink.href}`} onClick={() => setIsOpened(false)}>
+                <div className={styles.menuLabel}>
+                  {navLink.icon}
+                  <p>{navLink.label}</p>
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="#fbd858"
+                  className="size-6"
+                  height={35}
+                  width={35}
+                  aria-label="Navigate to section"
+                >
+                  <path strokeLinecap="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      )
     </>
   );
 }
