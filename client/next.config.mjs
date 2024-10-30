8361f8ba8367fd3abfd807de67ad2993e813b2cc
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-10-30 14:51:12 +0100

    Updated config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [{ type: "host", value: "klimatik.rs" }],
-        destination: "https://klimatik.rs/:path*",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
